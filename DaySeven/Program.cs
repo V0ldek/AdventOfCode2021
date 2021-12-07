325cc97d82ab23f594bfa201d2580279d3b756fa
Mateusz Gienieczko <mat@gienieczko.com>
2021-12-07 10:25:14 +0100

    Day 7.

---
+++ DaySeven/Program.cs
@@ -0,0 +1,40 @@
+﻿using Common;
+using Superpower;
+using Superpower.Parsers;
+
+new Runner().Run(
+    Numerics.IntegerInt32.ManyDelimitedBy(Character.EqualTo(',')),
+    values =>
+    {
+        var (low, high) = Median(values);
+        
+        var forLow = values.Select(x => Math.Abs(low - x)).Sum();
+        var forHigh = values.Select(x => Math.Abs(high - x)).Sum();
+
+        return Math.Min(forLow, forHigh);
+    },
+    values =>
+    {
+        var average = values.Average();
+        var low = (int)Math.Floor(average);
+        var high = (int)Math.Ceiling(average);
+
+        var forLow = values.Select(x => Math.Abs(low - x) * (Math.Abs(low - x) + 1) / 2).Sum();
+        var forHigh = values.Select(x => Math.Abs(high - x) * (Math.Abs(high - x) + 1) / 2).Sum();
+
+        return Math.Min(forLow, forHigh);
+    });
+
+static (int low, int high) Median(IEnumerable<int> xs)
+{
+    var list = xs.OrderBy(x => x).ToList();
+
+    if (list.Count % 2 == 0)
+    {
+        return (list[list.Count / 2], list[list.Count / 2]);
+    }
+    else
+    {
+        return (list[list.Count / 2], list[list.Count / 2 + 1]);
+    }
+}
